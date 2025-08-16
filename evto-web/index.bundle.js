28c0c3d61272a73478c33bf00a840366e2ce33a2
nickyonge <nick@duckpond.studio>
2025-08-15 17:31:09 -0700

    EvTo web test upload

---
+++ evto-web/index.bundle.js
@@ -0,0 +1,30 @@
+(self["webpackChunkweb"] = self["webpackChunkweb"] || []).push([["index"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+
+window.addEventListener('load', function () {
+    console.log("page loaded");
+    
+    var label = this.document.createElement("Label");
+    label.setAttribute("style", "font-weight:normal");
+    label.innerHTML = "Hello world!";
+    this.document.appendChild(label);
+    
+});
+
+console.log("test!");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EsQ0FBQztBQUNEO0FBQ0EscUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWIvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiXHJcbndpbmRvdy5hZGRFdmVudExpc3RlbmVyKCdsb2FkJywgZnVuY3Rpb24gKCkge1xyXG4gICAgY29uc29sZS5sb2coXCJwYWdlIGxvYWRlZFwiKTtcclxuICAgIFxyXG4gICAgdmFyIGxhYmVsID0gdGhpcy5kb2N1bWVudC5jcmVhdGVFbGVtZW50KFwiTGFiZWxcIik7XHJcbiAgICBsYWJlbC5zZXRBdHRyaWJ1dGUoXCJzdHlsZVwiLCBcImZvbnQtd2VpZ2h0Om5vcm1hbFwiKTtcclxuICAgIGxhYmVsLmlubmVySFRNTCA9IFwiSGVsbG8gd29ybGQhXCI7XHJcbiAgICB0aGlzLmRvY3VtZW50LmFwcGVuZENoaWxkKGxhYmVsKTtcclxuICAgIFxyXG59KTtcclxuXHJcbmNvbnNvbGUubG9nKFwidGVzdCFcIik7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
